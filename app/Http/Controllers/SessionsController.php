0f577251844945332bf2a64ae8f0097981e3db22
leoliang <leoliang@futunn.com>
2020-04-02 20:15:56 +0800

    添加session

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+
+        session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+        return redirect()->back()->withInput();
+    }
 }
