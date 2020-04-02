a6c64d096cf67334776d9c194cc3b8eaa6dabe7a
leoliang <leoliang@futunn.com>
2020-04-02 16:09:38 +0800

    前端资源添加版本号

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\workspace\demo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\workspace\demo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /data/demo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /data/demo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
