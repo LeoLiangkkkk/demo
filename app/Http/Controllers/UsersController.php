f31b879a128e6a0392e461f980f60d64c2361c09
leoliang <leoliang@futunn.com>
2020-04-02 19:17:48 +0800

    添加用户信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -16,5 +17,10 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
